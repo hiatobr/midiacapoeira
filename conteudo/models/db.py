ca0f51278061a987231fa359d085de8b76aaee9c
Hiato <hiato@riseup.net>
2014-04-18 10:50:53 -0300

    Removidas antigas tabelas obsoletas

--- conteudo/models/db.py
+++ conteudo/models/db.py
@@ -65,61 +65,6 @@ db.post_image.image_id.writable = db.post_image.image_id.readable = False
 ## /Tabelas para Imagens
 ## Tabelas para Textos
 db.define_table(
-	'text',
-	Field(
-		'body',
-		'text',
-		required=True,
-		notnull=True,
-	),
-	Field(
-		'date',
-		default=int(time.strftime('%s', time.localtime())),
-		update=int(time.strftime('%s', time.localtime())),
-		writable=False,
-		readable=True,
-		notnull=True,
-	),
-	Field(
-		'author',
-		default='Anônimx',
-	),
-	Field(
-		'fonte',
-	),
-	Field(
-		'licenca',
-	),
-	Field(
-		'email',
-	),
-)
-db.define_table(
-	'post_text',
-	Field(
-		'text_id',
-		'reference text',
-	),
-	Field(
-		'author',
-		default='Anônimx',
-	),
-	Field(
-		'email',
-	),
-	Field(
-		'body',
-		'text',
-		required=True,
-		notnull=True,
-	),
-)
-db.define_table(
-	'tag_text',
-	Field('text_id', 'reference text'),
-	Field('tag'),
-)
-db.define_table(
 	'texto',
 	Field(
 		'conteudo',
@@ -182,13 +127,7 @@ db.define_table(
 	),
 )
 
-db.text.body.requires = IS_NOT_EMPTY()
 db.texto.conteudo.requires = IS_NOT_EMPTY()
-db.post_text.body.requires = IS_NOT_EMPTY()
-db.post_text.text_id.requires = IS_IN_DB(db, db.image.id)
-db.post_text.text_id.readable = db.post_text.text_id.writable = False
-db.tag_text.text_id.requires = IS_IN_DB(db, db.text.id)
-db.tag_text.text_id.readable = db.tag_text.text_id.writable = False
 db.texto_post.conteudo.requires = IS_NOT_EMPTY()
 db.texto_post.texto_id.requires = IS_IN_DB(db, db.texto.id)
 db.texto_post.texto_id.readable = db.texto_post.texto_id.writable = False
