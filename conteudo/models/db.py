7ded341545132be2ae948f12784a6fbc73668da6
Hiato <hiato@riseup.net>
2014-04-18 11:19:54 -0300

    Removidas tabelas de imagens obsoletas

--- conteudo/models/db.py
+++ conteudo/models/db.py
@@ -16,49 +16,6 @@ crud = Crud(db)
 
 ## Tabelas para Imagens
 db.define_table(
-	'image',
-	Field(
-		'file',
-		'upload',
-		required=True,
-		uploadseparate=True,
-		notnull=True,
-	),
-	Field(
-		'date',
-		default=int(time.strftime('%s', time.localtime())),
-		update=int(time.strftime('%s', time.localtime())),
-		writable=False,
-		readable=True,
-		notnull=True,
-	),
-)
-db.define_table(
-	'post_image',
-	Field(
-		'image_id',
-		'reference image',
-	),
-	Field(
-		'author',
-		default='Anônimx',
-	),
-	Field(
-		'email',
-	),
-	Field(
-		'body',
-		'text',
-		required=True,
-		notnull=True,
-	),
-)
-db.define_table(
-	'tag_image',
-	Field('image_id', 'reference image'),
-	Field('tag'),
-)
-db.define_table(
 	'imagem',
 	Field(
 		'arquivo',
@@ -122,10 +79,6 @@ db.define_table(
 	),
 )
 
-db.tag_image.image_id.requires = IS_IN_DB(db, db.image.id)
-db.post_image.image_id.requires = IS_IN_DB(db, db.image.id)
-db.tag_image.image_id.writable = db.tag_image.image_id.readable = False
-db.post_image.image_id.writable = db.post_image.image_id.readable = False
 db.imagem.arquivo.requires = IS_NOT_EMPTY()
 db.imagem_post.conteudo.requires = IS_NOT_EMPTY()
 db.imagem_tag.imagem_id.requires = IS_IN_DB(db, db.imagem.id)
