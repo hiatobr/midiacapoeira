b0015d3e1c532b76953a09d965346b2f71a098d3
Hiato <hiato@riseup.net>
2014-06-17 03:33:44 -0300

    Modificado para usar variável com título

--- controllers/midiacapoeira.py
+++ controllers/midiacapoeira.py
@@ -2,6 +2,6 @@
 # this file is released under public domain and you can use without limitations
 
 def index():
-	response.flash = T("Seja bem vindx à midiacapoeira.in")
-	return dict(message=T('Seja bem vindx à midiacapoeira.in'))
+	response.flash = T('Seja bem vindx à ') + response.title
+	return dict(message = T('Seja bem vindx à ') + response.title)
 
