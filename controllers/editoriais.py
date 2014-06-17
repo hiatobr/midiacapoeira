cc1845372d2365dbc1fab4ef2297a6167870c819
Hiato <hiato@riseup.net>
2014-06-17 15:35:59 -0300

    Adicionando editoriais

---
+++ controllers/editoriais.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+def index():
+	response.flash = T("Editoriais")
+	return dict(message=T('Editoriais'))
+
